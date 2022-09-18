0f03101e5da7f47ddd6607df56a0065caa5db3a5
souvikpal2000 <spal62588@gmail.com>
2022-09-18 09:20:15 +0530

    Pushed Uncommited File

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,56 +1,110 @@
 const rows = document.querySelectorAll("tbody tr");
 const deleteBtn = document.querySelector(".delete");
+const editBtn = document.querySelector(".edit");
 let selectCount = 0;
-deleteBtn.classList.add("disable");
-deleteBtn.disabled = true;
 rows.forEach((row) => {
     row.addEventListener("click", () => {
         const tds = row.querySelectorAll("td");
         const checkBox = tds[0].querySelector("input");
-    
         if (checkBox.checked) {
             checkBox.checked = false;
             row.classList.remove("trCheck");
-            selectCount--;
+            --selectCount;
             if (selectCount == 0){
                 deleteBtn.disabled = true;
                 deleteBtn.classList.add("disable");
+				editBtn.disabled = true;
+                editBtn.classList.add("disable");
             }
-            return;
+            if(selectCount == 1){
+                editBtn.disabled = false;
+                editBtn.classList.remove("disable");
+            }
+			return;
         }
         checkBox.checked = true;
         row.classList.add("trCheck");
     
-        selectCount++;
+        ++selectCount;
         deleteBtn.disabled = false;
         deleteBtn.classList.remove("disable");
+		if(selectCount > 1){
+            editBtn.disabled = true;
+            editBtn.classList.add("disable");
+            return;
+	    }
+        editBtn.disabled = false;
+        editBtn.classList.remove("disable");
     });
 });
+
+//Delete Modal
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.querySelector(".delete");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var dlt = document.querySelector(".cancel");
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  	modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 dlt.onclick = function(){
     modal.style.display = "none";
 }
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
+
+//Edit Modal
+var editModal = document.querySelector(".editModal");
+var saveBtn = document.querySelector(".edit");
+var span01 = document.getElementsByClassName("close")[1];
+var cancelBtn = document.querySelector(".cancelModal");
+saveBtn.onclick = function() {
+	editModal.style.display = "block";
+}
+cancelBtn.onclick = function(){
+  	editModal.style.display = "none";
+}
+span01.onclick = function() {
+  	editModal.style.display = "none";
+}
+window.onclick = function(event) {
+  	if (event.target == editModal) {
+	  	editModal.style.display = "none";
+  	}
+}
+
+//Remove Cross Icon in Search Bar
+function hideSearch(){
+    document.querySelector(".searchIcon").classList.add("hideSearch");
+}
+
+//Add Modal
+var addModal = document.querySelector(".addModal");
+var add = document.querySelector(".add");
+var span02 = document.getElementsByClassName("close")[2];
+var cancelAddBtn = document.querySelector(".cancelAddModal");
+var clear = document.querySelector(".clear")
+add.onclick = function() {
+	addModal.style.display = "block";
+}
+cancelAddBtn.onclick = function(){
+    document.querySelector(".addForm").reset();
+    addModal.style.display = "none";
+}
+span02.onclick = function() {
+    document.querySelector(".addForm").reset();
+    addModal.style.display = "none";
+}
+clear.onclick = function() {
+    document.querySelector(".addForm").reset();
+}
+window.onclick = function(event) {
+  	if (event.target == addModal) {
+        addModal.style.display = "none";
+  	}
 }
